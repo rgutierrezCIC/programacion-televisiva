e25dd21fcad0ae37fc49363affbe0a5a47703cd4
pablosainz-cic <emailprueba@hotmail.com>
2024-09-02 10:47:03 +0200

    Commit para pasarlo a la nueva rama Pablo

---
+++ src/main/java/es/cic/grupo1/programacion_televisiva/model/Programa.java
@@ -0,0 +1,122 @@
+package es.cic.grupo1.programacion_televisiva.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Programa {
+     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String nombre;
+    private String descripcion;
+    private String clasificacion;
+    private String canal;
+
+    private LocalDateTime fechaIni;
+    private LocalDateTime fechaFin;
+
+    private boolean semanal;
+
+    
+    @Column(name = "dias", nullable = true)
+    private List<String> dias; // nullable by default
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_programa_id")
+    private TipoPrograma tipoPrograma;
+
+    // Getters and Setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getCanal() {
+        return canal;
+    }
+
+    public void setCanal(String canal) {
+        this.canal = canal;
+    }
+
+    public LocalDateTime getFechaIni() {
+        return fechaIni;
+    }
+
+    public void setFechaIni(LocalDateTime fechaIni) {
+        this.fechaIni = fechaIni;
+    }
+
+    public LocalDateTime getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDateTime fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public boolean isSemanal() {
+        return semanal;
+    }
+
+    public void setSemanal(boolean semanal) {
+        this.semanal = semanal;
+    }
+
+    public List<String> getDias() {
+        return dias;
+    }
+
+    public void setDias(List<String> dias) {
+        this.dias = dias;
+    }
+
+    public TipoPrograma getTipoPrograma() {
+        return tipoPrograma;
+    }
+
+    public void setTipoPrograma(TipoPrograma tipoPrograma) {
+        this.tipoPrograma = tipoPrograma;
+    }
+}
