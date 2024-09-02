f8181770970a2feed18676731e4ddc916f238d9f
pablosainz-cic <emailprueba@hotmail.com>
2024-09-02 08:55:17 +0200

    Primer commit, creacción proyecto back

---
+++ src/main/java/es/cic/grupo1/programacion_televisiva/ProgramacionTelevisivaApplication.java
@@ -0,0 +1,13 @@
+package es.cic.grupo1.programacion_televisiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionTelevisivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacionTelevisivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cic/grupo1/programacion_televisiva/ProgramacionTelevisivaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.grupo1.programacion_televisiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionTelevisivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
