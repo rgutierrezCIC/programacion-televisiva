38a634411ff9982b19c2c6f3bc754f698cfd2a06
pablosainz-cic <emailprueba@hotmail.com>
2024-09-03 08:38:51 +0200

    Autowired

--- src/main/java/es/cic/grupo1/programacion_televisiva/controller/ProgramaController.java
+++ src/main/java/es/cic/grupo1/programacion_televisiva/controller/ProgramaController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,7 @@ import es.cic.grupo1.programacion_televisiva.service.ProgramaService;
 @RequestMapping("/api/programas")
 public class ProgramaController {
 
+    @Autowired
     private  ProgramaService programaService;
 
 
